d651daac93e49265bc3cd33671d453aabf14c135
Natasha Dalal <natashadalal@gmail.com>
2014-03-21 11:25:15 -0400

    Initial Commit... Working basic ray tracer

---
+++ light_source.cpp
@@ -0,0 +1,55 @@
+/***********************************************************
+     Starter code for Assignment 3
+
+     This code was originally written by Jack Wang for
+		    CSC418, SPRING 2005
+
+		implements light_source.h
+
+***********************************************************/
+
+#include <cmath>
+#include <ctgmath>
+#include "light_source.h"
+
+void PointLight::shade( Ray3D& ray ) {
+	// TODO: implement this function to fill in values for ray.col
+	// using phong shading.  Make sure your vectors are normalized, and
+	// clamp colour values to 1.0.
+	//
+	// It is assumed at this point that the intersection information in ray
+	// is available.  So be sure that traverseScene() is called on the ray
+	// before this function.
+
+	Colour KA = (*ray.intersection.mat).ambient;
+	Colour KD = (*ray.intersection.mat).diffuse;
+	Colour KS = (*ray.intersection.mat).specular;
+	double alpha = (*ray.intersection.mat).specular_exp;
+
+	Colour IA = _col_ambient;
+	Colour ID = _col_diffuse;
+	Colour IS = _col_specular;
+
+	Vector3D N = (ray.intersection.normal);
+	N.normalize();
+
+	Vector3D L = (_pos - ray.intersection.point);
+	L.normalize();
+
+	Vector3D V = (-ray.dir);
+	V.normalize();
+
+	Vector3D R = ((2 * (L.dot(N)) * N) - L);
+	R.normalize();
+
+	double max1 = fmax(0.0, N.dot(L));
+
+	double vr_alpha = pow(V.dot(R), alpha);
+	double max2 = fmax(0.0, vr_alpha);
+
+	Colour shade = KA * IA + KD * (max1 * ID) + KS * (max2 * IS);
+	shade.clamp();
+	ray.col = shade;
+
+}
+
---
+++ scene_object.cpp
@@ -0,0 +1,137 @@
+/***********************************************************
+     Starter code for Assignment 3
+
+     This code was originally written by Jack Wang for
+		    CSC418, SPRING 2005
+
+		implements scene_object.h
+
+***********************************************************/
+
+#include <cmath>
+#include <iostream>
+#include "scene_object.h"
+
+bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
+		const Matrix4x4& modelToWorld ) {
+	// TODO: implement intersection code for UnitSquare, which is
+	// defined on the xy-plane, with vertices (0.5, 0.5, 0),
+	// (-0.5, 0.5, 0), (-0.5, -0.5, 0), (0.5, -0.5, 0), and normal
+	// (0, 0, 1).
+	//
+	// Your goal here is to fill ray.intersection with correct values
+	// should an intersection occur.  This includes intersection.point,
+	// intersection.normal, intersection.none, intersection.t_value.
+	//
+	// HINT: Remember to first transform the ray into object space
+	// to simplify the intersection test.
+	Point3D origin = worldToModel * ray.origin;
+	Vector3D dir = worldToModel * ray.dir;
+
+	double t = -origin[2]/dir[2];
+
+	if (t <= 0) {
+		return false;
+	}
+
+	double x_check = origin[0] + (t * dir[0]);
+	double y_check = origin[1] + (t * dir[1]);
+	Point3D intersectionPoint(x_check, y_check, 0);
+
+	if ((x_check <= 0.5 && x_check >= -0.5) &&
+		(y_check <= 0.5 && y_check >= -0.5)) {
+
+		// An intersection has occurred.
+		if (ray.intersection.none ||
+			t < ray.intersection.t_value) {
+			// We need to update the intersection.
+
+			Vector3D intersectionNormal = transNorm(
+											worldToModel,
+											Vector3D(0,0,1));
+
+			ray.intersection.t_value = t;
+			ray.intersection.point = modelToWorld * intersectionPoint;
+			ray.intersection.normal = intersectionNormal;
+			ray.intersection.none = false;
+
+			return true;
+
+		}
+	}
+
+	return false;
+}
+
+bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
+		const Matrix4x4& modelToWorld ) {
+	// TODO: implement intersection code for UnitSphere, which is centred
+	// on the origin.
+	//
+	// Your goal here is to fill ray.intersection with correct values
+	// should an intersection occur.  This includes intersection.point,
+	// intersection.normal, intersection.none, intersection.t_value.
+	//
+	// HINT: Remember to first transform the ray into object space
+	// to simplify the intersection test.
+
+	Point3D origin = worldToModel * ray.origin;
+	Vector3D dir = worldToModel * ray.dir;
+
+	Point3D center(0,0,0);
+	double radius = 1;
+
+	double a = dir.dot(dir);
+	double b = (2*(origin - center)).dot(dir);
+	double c = ((origin-center).dot((origin-center)) - radius*radius);
+
+	double discriminant = (b*b) - (4*a*c);
+
+	if (discriminant < 0) {
+		return false;
+	}
+
+	double t0 = (-b - sqrt(discriminant))/(2*a);
+	double t1 = (-b + sqrt(discriminant))/(2*a);
+	double t = t0;
+
+	if (t0 > t1) {
+		//ensure that t0 is smaller.
+		double temp = t0;
+		t0 = t1;
+		t1 = temp;
+	}
+
+	if (t1 < 0) {
+		return false;
+	}
+
+	if (t0 < 0) {
+		t = t1;
+	}
+
+	if (ray.intersection.none ||
+		t < ray.intersection.t_value) {
+		//update the intersection.
+
+		double x = origin[0] + t*dir[0];
+		double y = origin[1] + t*dir[1];
+		double z = origin[2] + t*dir[2];
+
+		Point3D intersectionPoint(x,y,z);
+
+		Vector3D intersectionNormal = transNorm(
+											worldToModel,
+											Vector3D(2*x, 2*y, 2*z));
+
+		ray.intersection.t_value = t;
+		ray.intersection.point = modelToWorld * intersectionPoint;
+		ray.intersection.normal = intersectionNormal;
+		ray.intersection.none = false;
+
+		return true;
+	}
+
+	return false;
+}
+
